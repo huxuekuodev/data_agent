ba28127394da94f5e3acf068234ebbb4b25ecb06
hu <18233110173@163.com>
2026-06-17 17:33:42 +0800

    更换embedding模型与向量数据库

--- app/agent/context.py
+++ app/agent/context.py
@@ -1,18 +1,19 @@
 from typing import TypedDict
 
-from langchain_huggingface import HuggingFaceEndpointEmbeddings
-
+from app.clients.siliconflow_embeding_client import PureRequestsEmbeddings
 from app.repositories.es.value_es_repository import ValueESRepository
+from app.repositories.milvus.column_milvus_repository import DataAgentColumnCollection
+from app.repositories.milvus.metric_milvus_repository import MetricMilvusRepository
 from app.repositories.mysql.dw.dw_mysql_repository import DWMySQLRepository
 from app.repositories.mysql.meta.meta_mysql_repository import MetaMySQLRepository
-from app.repositories.qdrant.column_qdrant_repository import ColumnQdrantRepository
-from app.repositories.qdrant.metric_qdrant_repository import MetricQdrantRepository
 
 
 class DataAgentContext(TypedDict):
-    embedding_client: HuggingFaceEndpointEmbeddings
-    column_qdrant_repository: ColumnQdrantRepository
+    embedding_client: PureRequestsEmbeddings
+    # column_qdrant_repository: ColumnQdrantRepository
+    column_milvus_repository: DataAgentColumnCollection
     value_es_repository: ValueESRepository
-    metric_qdrant_repository: MetricQdrantRepository
+    # metric_qdrant_repository: MetricQdrantRepository
+    metric_milvus_repository: MetricMilvusRepository
     meta_mysql_repository: MetaMySQLRepository
     dw_mysql_repository: DWMySQLRepository
