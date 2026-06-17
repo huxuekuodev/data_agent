3217c8618c98a3faf98af7e90bcb05051cf65594
hu <18233110173@163.com>
2026-06-17 17:24:58 +0800

    更新llm配置

--- app/agent/llm.py
+++ app/agent/llm.py
@@ -1,14 +1,22 @@
 from langchain.chat_models import init_chat_model
 
 from app.conf.app_config import app_config
+from app.core.decorators import timing
 
-llm = init_chat_model(model=app_config.llm.model_name,
-                      model_provider="openai",
-                      api_key=app_config.llm.api_key,
-                      base_url=app_config.llm.base_url,
-                      temperature=0)
+llm = init_chat_model(
+    model=app_config.llm.model_name,
+    model_provider="openai",
+    api_key=app_config.llm.api_key,
+    base_url=app_config.llm.base_url,
+    temperature=0,
+)
 
 
-if __name__ == '__main__':
-    for chunk in llm.stream("What is the meaning of life?"):
-        print(chunk.text)
+@timing
+def test():
+    response = llm.invoke("你好")
+    print(response.content)
+
+
+if __name__ == "__main__":
+    test()
