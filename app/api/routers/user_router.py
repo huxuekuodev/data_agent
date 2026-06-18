8e3d82e11c473aae991f7e57a87867b3bdd8785f
hu <18233110173@163.com>
2026-06-18 17:26:26 +0800

    加入jwt

--- app/api/routers/user_router.py
+++ app/api/routers/user_router.py
@@ -1,3 +1,6 @@
+from datetime import datetime, timedelta
+
+import jwt
 from fastapi import APIRouter
 
 from app.api.shemas.user_schema import UserLoginSchema
@@ -15,4 +18,12 @@ async def getUserInfo():
 @user_router.post("/user/login")
 async def Login(login_schema: UserLoginSchema):
     # 登录逻辑
-    return {"message": "登录成功"}
+    jwt_token = jwt.encode(
+        {
+            "username": login_schema.username,
+            "exp": datetime.now() + timedelta(days=15),
+        },
+        "secret",
+        algorithm="HS256",
+    )
+    return {"message": "登录成功", "token": jwt_token}
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from loguru import logger
 
 from app.api.routers.query_router import query_router
+from app.api.routers.user_router import user_router
 
 app = FastAPI()
 app.include_router(query_router)
+app.include_router(user_router)
